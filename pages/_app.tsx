52438924e94c363b4f02536852ba0efb44e9b63d
theGobindSingh <android.gobind@gmail.com>
2022-12-20 15:37:37 +0530

    font explaination

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/styles.css";
+import type { AppProps } from "next/app";
+import localFont from "@next/font/local";
+// import { useEffect } from "react";
+import Head from "next/head";
+
+const kato = localFont({
+  src: "../public/assets/fonts/kato.ttf",
+  fallback: ["sans-serif"],
+  weight: "400",
+  variable: "--font-kato",
+  preload: true,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // method 2
+  // useEffect(() => {
+  //   document.getElementById("__next")?.classList.add(kato.className);
+  //   console.log(kato.style);
+  // }, []);
+  return (
+    <>
+      <Head>
+        <style className="dynamic-fonts">
+          {
+            //method 1
+            `:root{   --font-kato: ${kato.style.fontFamily};   }`
+          }
+        </style>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
